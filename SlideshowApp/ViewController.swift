26117b58d48d45f0c04c0e929f2e93fb2d5c6e94
ryoutarouakou <allergy.blue@gmail.com>
2017-12-19 05:30:59 +0900

    画像とアニメーションの設定

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var View1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 【1】 画像データをインスタンス化
+        let image1: UIImage! = UIImage(named: "Image1")
+        let image2: UIImage! = UIImage(named: "Image2")
+        let image3: UIImage! = UIImage(named: "Image3")
+        // 【2】 コマ送りに使う画像データの配列をセット
+        View1.animationImages = [image1, image2, image3]
+        // 【3】 コマ送りの間隔を設定
+        View1.animationDuration = 1
+        // 【4】 コマ送りのアニメーションを開始
+        View1.startAnimating()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Susumu(_ sender: Any) {
+    }
+    
+    @IBAction func Modoru(_ sender: Any) {
+    }
+    
 }
 
