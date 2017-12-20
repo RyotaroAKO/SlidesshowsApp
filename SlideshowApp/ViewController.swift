9636eb62238d2a0aff4715a163a3097570ceb165
ryoutarouakou <allergy.blue@gmail.com>
2017-12-20 22:11:07 +0900

    ・スライドショーを実行すると、初回が4秒後になっていたので、初回も2秒後になるように修正。ifとelseの前に、currentpage+=1とした。 ・スライドショー中に拡大表示するとバグっていたので修正。timer.invalidateを追加。 ・再生ボタンと停止ボタンを分離

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -16,19 +16,22 @@ class ViewController: UIViewController {
     // 現在のスライドショーのステータス　0=停止、1＝再生
     var CurrentPage = 0
     // 現在のPage番号　0~2
-    var imageArray = [UIImage]()
     var timer = Timer()
     
+    let image0: UIImage! = UIImage(named: "Image0")
+    let image1: UIImage! = UIImage(named: "Image1")
+    let image2: UIImage! = UIImage(named: "Image2")
+    
+    var imageArray = [UIImage]()
+    
+    let Saiseiimage: UIImage! = UIImage(named: "Saisei")
+    let Teishiimage: UIImage! = UIImage(named: "Teishi")
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let image0: UIImage! = UIImage(named: "Image0")
-        let image1: UIImage! = UIImage(named: "Image1")
-        let image2: UIImage! = UIImage(named: "Image2")
-        
         imageArray = [image0, image1, image2]
-        
         View1.image = imageArray[CurrentPage]
+        SaiseiTeishibutton.setImage(Saiseiimage, for: UIControlState())
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,31 +75,41 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    @IBOutlet weak var SaiseiTeishibutton: UIButton!
+    
     @IBAction func SaiseiTeishi(_ sender: Any) {
         switch Status {
         case 0:
             Status = 1
+            SaiseiTeishibutton.setImage(Teishiimage, for: UIControlState())
             //スライドショー開始。2秒間隔でタイマー起動
             timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { (timer) in
-                //タイマー起動ごとに、配列から画像を取り出して、View1に反映していく。ここはif文にしてみた。
+                //タイマー起動ごとに、ページ番号追加。配列から画像を取り出して、View1に反映していく。ここはif文にしてみた。
+                self.CurrentPage = self.CurrentPage + 1
                 if(self.CurrentPage > 2){
                     self.CurrentPage = 0
                     self.View1.image = self.imageArray[self.CurrentPage]
                 }else{
                     self.View1.image = self.imageArray[self.CurrentPage]
                 }
-                self.CurrentPage = self.CurrentPage + 1
             })
             
         case 1:
             Status = 0
+            SaiseiTeishibutton.setImage(Saiseiimage, for: UIControlState())
             // スライドショー停止
             timer.invalidate()
+            
         default:break
         }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        Status = 0
+        SaiseiTeishibutton.setImage(Saiseiimage, for: UIControlState())
+        // スライドショー停止
+        timer.invalidate()
         // segueから遷移先のViewController2を取得する
         let Controller2:ViewController2 = segue.destination as! ViewController2
         // 遷移先のViewController2で宣言しているKakudaiimageに画像を代入して渡す
